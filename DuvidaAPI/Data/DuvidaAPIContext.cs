76ba46e74eb81cdb364213b464b819f5ac3f01ca
cauematheus23 <cauematheus_23@hotmail.com>
2024-06-06 16:50:39 -0300

    Add project files.

---
+++ DuvidaAPI/Data/DuvidaAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DuvidaAPI.Data
+{
+    public class DuvidaAPIContext : DbContext
+    {
+        public DuvidaAPIContext (DbContextOptions<DuvidaAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Duvidas> Duvidas { get; set; } = default!;
+    }
+}
---
+++ OportunidadeAPI/Data/OportunidadeAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace OportunidadeAPI.Data
+{
+    public class OportunidadeAPIContext : DbContext
+    {
+        public OportunidadeAPIContext (DbContextOptions<OportunidadeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Oportunidade> Oportunidade { get; set; } = default!;
+    }
+}
